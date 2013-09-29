9b0a68f496e37d064fd89e4487c7a21751e14acd
xho <xhoxho@gmail.com>
2013-09-29 18:09:33 +0200

    removed youtube, needs fix

--- js/earify.js
+++ js/earify.js
@@ -149,11 +149,13 @@ var earify = {
 			}
 
 			// youtube video
+			/*
 			if (earify.tweet.videourl) {
 				var embed = '<object width="300" height="225"><param name="movie" value="' + earify.tweet.videourl + '&amp;version=3"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="' + earify.tweet.videourl + '&amp;version=3" type="application/x-shockwave-flash" width="300" height="225" allowscriptaccess="always" allowfullscreen="true"></embed></object>';
 	            $("#video").append(embed);
 				$('#video').slideDown(2000);
 			}
+			*/
 		});
 
 
