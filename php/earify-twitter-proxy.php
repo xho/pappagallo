6352e0260564a59c167b39413c61d741f0af593f
xho <xho@milla.bedita.net>
2013-10-01 18:15:14 +0200

    Close #3 php proxy server side edit

--- php/earify-twitter-proxy.php
+++ php/earify-twitter-proxy.php
@@ -1,13 +1,33 @@
 <?php
-	session_start();
-	require_once("twitteroauth/twitteroauth/twitteroauth.php"); //Path to twitteroauth library
+	if (empty($_GET["excludereplies"]) || $_GET["excludereplies"] == "false") {
+		$excludereplies = "false"; 
+	} else {
+		$excludereplies = "true"; 
+	}
 
-	$twitteruser = "user"; //user name of twitter user using this service (owner of the app)
+	if (empty($_GET["excludert"]) || $_GET["excludert"] == "false") {
+		$includeReplies = "true"; 
+	} else {
+		$includeReplies = "false"; 
+	}
+
+	if (empty($_GET["user"])) {
+		$twitteruser = "riotta"; 
+	} else {
+		$twitteruser = $_GET["user"];
+	}
+
+
+
+	session_start();
+	require_once("twitteroauth/twitteroauth/twitteroauth.php"); //Path to twitteroauth library you downloaded in step 3
+
+	// $twitteruser = "riotta"; //user name you want to reference
 	$notweets = 1; //how many tweets you want to retrieve
-	$consumerkey = "12345"; //Noted keys from step 2
-	$consumersecret = "123456789"; //Noted keys from step 2
-	$accesstoken = "123456789"; //Noted keys from step 2
-	$accesstokensecret = "12345"; //Noted keys from step 2
+	$consumerkey = "haudswXMv6tFaPzHB26w"; //Noted keys from step 2
+	$consumersecret = "ksPKVKBcccLYaGHf5NE1i8DeBuWEkLsUn3yY7Nb9HZY"; //Noted keys from step 2
+	$accesstoken = "81815107-paAJK4ne226KBMqjL9HPDVzALKm2R66LhVQKBZLID"; //Noted keys from step 2
+	$accesstokensecret = "5IxBdbQsOAbxpFxQHPPmMjDS5dmwenXSpE834pSqbE"; //Noted keys from step 2
 
 	function getConnectionWithAccessToken($cons_key, $cons_secret, $oauth_token, $oauth_token_secret) {
 	  $connection = new TwitterOAuth($cons_key, $cons_secret, $oauth_token, $oauth_token_secret);
@@ -16,13 +36,19 @@
 
 	$connection = getConnectionWithAccessToken($consumerkey, $consumersecret, $accesstoken, $accesstokensecret);
 
-	$tweets = $connection->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=".$twitteruser."&count=".$notweets);
+	$getURL = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=" . $twitteruser .
+			"&count=" . $notweets .
+			"&include_rts=" . $includeReplies .
+			"&exclude_replies=" . $excludereplies;
 
-    if (!empty($tweets[0])) {                                                                                                                                     
-        echo json_encode($tweets[0]);                                                                                                                             
-        // echo "<pre>";                                                                                                                                          
-        // print_r($tweets); //testing remove for production                                                                                                      
-    } else {                                                                                                                                                      
-        die();                                                                                                                                                    
-    }  
+	$tweets = $connection->get($getURL);
+
+	if (!empty($tweets[0])) {
+		echo json_encode($tweets[0]);
+		// echo "<pre>";
+		// print_r($tweets); //testing remove for production
+	} else {
+		die();
+	}
 ?>
+
